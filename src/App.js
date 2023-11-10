b825a9b1a10a4e92f5543404d210bf787aa7bf7f
Sandesh Neupane <sandeshpkr977@gmail.com>
2023-11-10 17:15:54 +0545

    modification

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import AdminPanel from "./AdminPanel";
+import LoginForm from "./LoginForm";
 
-function App() {
+function MyButton() {
+  return <button>Click me!</button>;
+}
+
+export function MyApp() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>welcome to my react app</h1>
+      <MyButton />
+      <br></br>
+      <img className="avtar" src="#" alt="avtar" />
+    </>
   );
 }
 
-export default App;
+const user = {
+  name: "alu",
+  imageUrl:
+    "https://scontent.cdninstagram.com/v/t51.2885-19/276086747_544735596870900_4344961830561948454_n.jpg?stp=dst-jpg_s100x100&_nc_cat=108&ccb=1-7&_nc_sid=c4dd86&_nc_ohc=O9a8ldQigKkAX_8UpoQ&_nc_oc=AQldQHTLRvBWqnK157__ZMLfOXpbJb3MpIJyBUUDbkW1plCqeEkICEEi5PiAEuipABSPTiyaIstPDcfvbSWRhx7O&_nc_ad=z-m&_nc_cid=0&_nc_ht=scontent.cdninstagram.com&oh=00_AfBEHbqHdxzwcTrxT7En_NA_tqyD9KclkiGtaZeHrCPj4Q&oe=65521E8C",
+  imageSize: 90,
+};
+
+export function Profile() {
+  return (
+    <>
+      <h1>{user.name}</h1>
+      <img
+        className="avatar"
+        src={user.imageUrl}
+        alt={"photo of " + user.name}
+        style={{
+          width: user.imageSize,
+          height: user.imageSize,
+        }}
+      />
+    </>
+  );
+}
+
+const component = ({ isLoggedIn }) => {
+  let content;
+  if (isLoggedIn) {
+    content = <AdminPanel />;
+  } else {
+    content = <LoginForm />;
+  }
+  return <div>{content}</div>;
+};
+export default component;
